721f4ce1ff9a36372cd57b587f5c356709fe08d3
Chimmysmallz <105133066+Chimmysmallz@users.noreply.github.com>
2023-01-16 14:24:22 +0100

    Update 2-export_to_JSON.py

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,27 +1,19 @@
 #!/usr/bin/python3
+"""Exports to-do list information for a given employee ID to JSON format."""
 import json
 import requests
-from sys import argv
-"""
-accessing a url with employee ID to return information
-"""
-
+import sys
 
 if __name__ == "__main__":
-    ID = argv[1]
-    user = requests.get("https://jsonplaceholder.typicode.com/users/{}".
-                        format(ID), verify=False).json()
-    todo = requests.get("https://jsonplaceholder.typicode.com/todos?ID={}".
-                        format(ID), verify=False).json()
-    username = user.get('username')
-    tasks = []
-    for task in todo:
-        task_dict = {}
-        task_dict["task"] = task.get('title')
-        task_dict["completed"] = task.get('completed')
-        task_dict["username"] = username
-        tasks.append(task_dict)
-    json_file = {}
-    json_file[ID] = tasks
-    with open("{}.json".format(ID), 'w') as jsfile:
-        json.dump(json_file, jsfile)
+    user_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(user_id)).json()
+    username = user.get("username")
+    todos = requests.get(url + "todos", params={"userId": user_id}).json()
+
+    with open("{}.json".format(user_id), "w") as jsonfile:
+        json.dump({user_id: [{
+                "task": t.get("title"),
+                "completed": t.get("completed"),
+                "username": username
+            } for t in todos]}, jsonfile)
