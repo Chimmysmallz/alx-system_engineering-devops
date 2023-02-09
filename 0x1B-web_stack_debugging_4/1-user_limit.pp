959c4ae76461fccb4a2634213c3cd2bfd7cecb81
Chimmysmallz <105133066+Chimmysmallz@users.noreply.github.com>
2023-02-09 19:29:57 +0100

    Update 1-user_limit.pp

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,1 +1,6 @@
 #!/usr/bin/env bash
+#change ULIMIT
+exec { 'Change ULIMIT':
+  command  => 'echo -e "holberton hard nofile 2500\nholberton soft nofile 25000" > /etc/security/limits.conf',
+  provider => shell,
+}
